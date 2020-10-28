11ffd48086e7d72bbfcb166d67808232cc9a8a52
Faraaz2633 <mdfaraz4343@gmail.com>
2020-10-28 22:22:26 +0530

    New Changes has been made

--- coolj.cpp
+++ coolj.cpp
@@ -1,1 +1,29 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+//This code is to find Armstrong Number
+int main(){
 
+    int n;
+    cin>>n;
+
+    int sum = 0;
+    int originaln = n;
+
+    while(n>0){
+        int lastdigit = n%10;
+        sum += pow(lastdigit,3);
+        n = n/10 ;
+    }
+
+    if(sum==originaln){
+        cout<<"Armstrong Number"<<endl;
+    }
+
+    else{
+        cout<<"This is Not an Armstrong Number"<<endl;
+    }
+
+
+    return 0;
+}
